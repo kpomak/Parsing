ea2dfca2540e30d41fb49e2574494b53e42145cd
kpomak <romka_ne@mail.ru>
2023-01-09 01:52:42 +0300

    data list

--- beautysoup.py
+++ beautysoup.py
@@ -4,18 +4,17 @@ import requests
 import re
 
 
-def salary_parser(salary: str) -> tuple:
-    salary = salary.replace('\u202f', '').replace(' ', '')
-    currency = re.findall(r'\D+', salary)[-1]
-    money = re.findall(r'\d+', salary)
+def salary_parser(salary: str) -> list:
+    salary = salary.replace("\u202f", "").replace(" ", "")
+    currency = re.findall(r"\D+", salary)[-1]
+    money = re.findall(r"\d+", salary)
     min_salary = money[0]
-    max_salary = money[-1] if money[-1] != min_salary else 'infinity'
-    return min_salary, max_salary, currency
+    max_salary = money[-1] if money[-1] != min_salary else "infinity"
+    return [min_salary, max_salary, currency]
 
 
-def hh_parse(target: str, data: dict) -> dict:
+def hh_parse(target: str, data: list) -> list:
     url = "https://spb.hh.ru/search/vacancy"
-    source = "hh.ru"
     params = {
         "from": "suggest_post",
         "area": "2",
@@ -34,14 +33,24 @@ def hh_parse(target: str, data: dict) -> dict:
     vacancys = soup.find_all(class_="vacancy-serp-item-body")
 
     for vacancy in vacancys:
-        salary = vacancy.find(attrs={'data-qa':"vacancy-serp__vacancy-compensation"})
-        *salary, = salary_parser(salary.getText()) if salary else ''
+        salary = vacancy.find(attrs={"data-qa": "vacancy-serp__vacancy-compensation"})
+        salary = salary_parser(salary.getText()) if salary else None
         vacancy = vacancy.find("a")
         name = vacancy.getText()
         link = vacancy.get("href")
 
+        parsed_vacancy = {"name": name, "link": link, "source": "hh.ru"}
+        if salary:
+            parsed_vacancy["salary"] = salary
+
+        parsed_data.append(parsed_vacancy)
+
     return data
 
-parsed_data = {}
 
-hh_parse("python", parsed_data)
+if __name__ == "__main__":
+    parsed_data = []
+
+    hh_parse("python", parsed_data)
+
+    pprint(parsed_data)
